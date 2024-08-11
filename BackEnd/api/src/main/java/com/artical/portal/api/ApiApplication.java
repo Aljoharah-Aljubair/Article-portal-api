17dce0d4ebc8eeeb4bb85004ba19193e467d7dea
Aljoharah Aljubair <116559876+Aljoharah-Aljubair@users.noreply.github.com>
2024-08-11 13:05:26 +0300

    Update ApiApplication.java

--- BackEnd/api/src/main/java/com/artical/portal/api/ApiApplication.java
+++ BackEnd/api/src/main/java/com/artical/portal/api/ApiApplication.java
@@ -1,20 +1,11 @@
 package com.artical.portal.api;
 
-import com.artical.portal.api.models.Privilege;
-import com.artical.portal.api.models.UserEntity;
-import com.artical.portal.api.repository.PrivilegeRepository;
-import com.artical.portal.api.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 
 @CrossOrigin(origins = "http://localhost:4200/")
 @SpringBootApplication
@@ -26,4 +17,3 @@ public class ApiApplication {
 	}
 
 	}
-
