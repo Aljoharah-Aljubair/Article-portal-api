c55c0b8435411e7acaa92fb5c1505f639c9e3676
Aljoharah Aljubair <116559876+Aljoharah-Aljubair@users.noreply.github.com>
2024-08-11 11:23:34 +0300

    Delete api directory

--- api/src/main/java/com/artical/portal/api/exceptions/ArticleOwnerException.java
+++
@@ -1,8 +0,0 @@
-package com.artical.portal.api.exceptions;
-
-public class ArticleOwnerException extends RuntimeException{
-    public ArticleOwnerException(String message){
-        super(message);
-    }
-
-}
--- api/src/main/java/com/artical/portal/api/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.artical.portal.api.service;
-
-import com.artical.portal.api.dto.UserDto;
-import com.artical.portal.api.models.UserEntity;
-
-public interface UserService {
-    UserDto registerUser(UserDto user);
-}
