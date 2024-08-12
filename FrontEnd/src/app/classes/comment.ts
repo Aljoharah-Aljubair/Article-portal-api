d5744a46632b7db4c15f2a645517d161beb3e82a
Aljoharah Aljubair <116559876+Aljoharah-Aljubair@users.noreply.github.com>
2024-08-12 09:43:51 +0300

    Delete FrontEnd/src directory

--- FrontEnd/src/app/classes/comment.ts
+++
@@ -1,9 +0,0 @@
-export class Comment {
-  constructor(
-    public id: number,
-    public content: string,
-    public userId: number,
-    public userUsername: string,
-    public articleId: number,
-    public createdAT: string) { };
-}
