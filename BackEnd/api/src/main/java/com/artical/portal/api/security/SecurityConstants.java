fba03998469e5527322e44714ae462aa2b37e45d
Aljoharah Aljubair <116559876+Aljoharah-Aljubair@users.noreply.github.com>
2024-08-15 11:13:34 +0300

    Delete BackEnd/api directory

--- BackEnd/api/src/main/java/com/artical/portal/api/security/SecurityConstants.java
+++
@@ -1,7 +0,0 @@
-package com.artical.portal.api.security;
-
-public class SecurityConstants {
-
-    public static final long JWT_EXPIRATION=700000;
-    public static final String JWT_SECRET = "secret";
-}
